4d1201e5d0d2edca756965e767e78629d648beed
Igor <igorferreiradaniel99@hotmail.com>
2022-08-08 19:16:03 -0300

    Feign client

--- hr-payroll/src/main/java/com/udemy/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/udemy/hrpayroll/services/PaymentService.java
@@ -1,35 +1,21 @@
 package com.udemy.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.udemy.hrpayroll.entities.Payment;
 import com.udemy.hrpayroll.entities.Worker;
+import com.udemy.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
-	//Usaremos o componente RestTemplate para fazer requisição para o projeto hr-worker;
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		//Iremos atribuir para a chave id o valor passado na requisição do PaymentResource (workerId):
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId); 
 		
-		//Após atribuído o valor para o "id", podemos usar essa chave para fazer a requisição dos dados do worker no hr-worker:
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
-		
-		//Por fim executar o serviço do hr-payroll em cima da entidade recebida do hr-worker;
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
